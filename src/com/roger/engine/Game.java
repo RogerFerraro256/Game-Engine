a75c38100b875b354b3a8e39e06c0b4dea0e97b6
roger <rogerferraro256@gmail.com>
2020-03-23 19:30:26 -0300

    add some shit

--- src/com/roger/engine/Game.java
+++ src/com/roger/engine/Game.java
@@ -1,17 +1,32 @@
 package com.roger.engine;
 
+public class Game implements Runnable {
 
-
-
-public class Game {
-
-	
 	public static int width = 300;
-	public static int height = width/16*9;
+	public static int height = width / 16 * 9;
 	public static int scale = 3;
+
+	private Thread thread;
+	private boolean running = false;
+
+	// synchronized = prevent problems
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+
+	}
 	
-	
-	
-	
-	
+	public void run() {
+		
+	}
+
 }
