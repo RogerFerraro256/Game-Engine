298ba3cabd760a40c968fc43908f358fd6c1ecc8
roger <rogerferraro256@gmail.com>
2020-03-23 19:48:32 -0300

    add some shit

--- src/com/roger/engine/Game.java
+++ src/com/roger/engine/Game.java
@@ -1,21 +1,42 @@
 package com.roger.engine;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
 
+	
+	
+	public Game() {
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+		
+		
+	}
+	
 	// synchronized = prevent problems
 	public synchronized void start() {
+		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
 
 	public synchronized void stop() {
+		running = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
@@ -24,8 +45,25 @@ public class Game implements Runnable {
 		}
 
 	}
-	
+
 	public void run() {
+		while (running) {
+			System.out.println("Runnig...");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Game");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		
+		game.start();
 		
 	}
 
